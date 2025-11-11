ded948abacf35c53803dc6e4e1a02cc4ee06d75f
Justin Jones Brey <justinjones.brey@wvsu.edu.ph>
2025-11-11 23:08:58 +0800

    added: dashboard ui and assets

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'onboarding.dart';
+import 'dashboard.dart';
 
 void main() {
   runApp(const MyApp());
@@ -21,7 +22,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
         fontFamily: 'Outfit',
       ),
-      home: const OnboardingScreen(),
+      home: const HomeScreen(),
     );
   }
 }
